cca6c3501b894dee764af654204a7176c7434a11
Audman <tigranohanyanv2@gmail.com>
2024-10-29 21:23:26 +0400

    Alaska 2

--- WalterInAlaska.java
+++ WalterInAlaska.java
@@ -7,11 +7,13 @@ public class WalterInAlaska<E>
         Position<E> rightSidePosition) // initially null
     {
         if(root != null && rightSidePosition == null && leftSidePosition == null)
-        {
-            Position<E> tempRoot = root;
-            root = null;
-            return Walter(tree, root, tree.left(root),tree.right(root));
-        }
+            return tree.left(root) != tree.right(root) &&
+                tree.left(root) != null &&
+                tree.right(root) != null &&
+                Walter(tree, null, tree.left(root),tree.right(root));
+
+        if(root == null && rightSidePosition == null && leftSidePosition == null)
+            return true;
 
         if (rightSidePosition == null ||
             leftSidePosition == null || // Check if only one of them is null
@@ -20,5 +22,16 @@ public class WalterInAlaska<E>
 
         return Walter(tree, root, tree.left(leftSidePosition),tree.right(rightSidePosition)) &&
                 Walter(tree, root, tree.left(rightSidePosition),tree.right(leftSidePosition));
+    }
+
+    public static void main(String[] args) {
+        LinkedBinaryTree<Integer> tree = new LinkedBinaryTree<>();
+        Position root = tree.addRoot(5);
+        Position l1 = tree.addLeft(root, 4);
+        Position l2 = tree.addRight(root,4);
+        Position r1 = tree.addLeft(l1, 3);
+        Position r2 = tree.addRight(l2,3);
+
+        System.out.println(new WalterInAlaska<Integer>().Walter(tree, root, null,null));
     }
 }
